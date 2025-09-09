ec6ddf58479c59f69f5521dc518876fc55fc0312
esteward0434 <eric.steward@siu.edu>
2025-09-08 20:21:26 -0500

    Letting user select their numbers and operation

--- simple-c-calc.c
+++ simple-c-calc.c
@@ -2,9 +2,18 @@
 
 int main (); 
 {
+ float num1, num2, result;
+ char operator;
+ printf ("Enter your first number\n");
+ scanf (%f, &num1);
+ printf ("Enter your second number\n");
+ scanf (%f, &num2);
 
+// Letting the user input the numbers they would like to calculate
 
+ printf ("Enter the operator you would like to use: ('+')")
+ scanf (%c, &operator)
 
-
-    return 0; 
+// Allows user to pick their operation
+ return 0; 
 }
