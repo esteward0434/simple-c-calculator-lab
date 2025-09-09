bcf215b803883b13f8b6f3ab33bbbae54d8f30f4
esteward0434 <eric.steward@siu.edu>
2025-09-08 20:24:58 -0500

    Syntax fixes

--- simple-c-calc.c
+++ simple-c-calc.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-int main (); 
-{
+int main () {
  float num1, num2, result;
  char operator;
  printf ("Enter your first number\n");
- scanf (%f, &num1);
+ scanf ("%f", &num1);
  printf ("Enter your second number\n");
- scanf (%f, &num2);
+ scanf ("%f", &num2);
 
 // Letting the user input the numbers they would like to calculate
 
- printf ("Enter the operator you would like to use: ('+')")
- scanf (%c, &operator)
+ printf ("Enter the operator you would like to use: ('+')");
+ scanf ("%c", &operator);
 
 // Allows user to pick their operation
  return 0; 
